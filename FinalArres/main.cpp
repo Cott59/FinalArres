950da1968d2ae2e938dfbd7f366886ad32e2f4e8
Cott59 <chugaevura@mail.ru>
2023-02-10 21:16:41 +0500

    4 зад -гот 5-начата

--- FinalArres/main.cpp
+++ FinalArres/main.cpp
@@ -7,7 +7,7 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	int n, m;
 	int sum;
-	std::cout << " Hello World! \n\n";
+	//std::cout << " Hello World! \n\n";
 	/*
 	//Сортировка половины массива
 	std::cout<< "задача 1 \n Изначальный массив \n";
@@ -56,7 +56,7 @@ int main() {
 	}
 	std::cout << " Кол-во дней, когда температура опускалась ниже " << n << " градусов " << counter << '\n';
 	std::cout << " Средняя температура за месяц " << (double)sum / size2 << "\n\n";
-	*/
+	
 
 	//задача 3
 	std::cout << "задача 3 \n  Вхождение в диапозон \n";
@@ -76,12 +76,33 @@ int main() {
 			std::cout << i + 1 << ". " << arr3[i] << '\n';
 	}
 	std::cout << " \n\n\n\n";
+	*/
+	
+	const int size4 = 10;
+	int arr4[size4];
+	srand(time(NULL));
+	std::cout << " Вывод : \n";
+	for (int i = 0; i < size4; i++) {
+		arr4[i] = rand() % 101;
+		std::cout << arr4[i] << '\t';
+	}
+	std::cout << " \n\n";
+	for (int i = 1; i < size4; i++)
+		if (arr4[i] > arr4[i - 1])
+			std::cout << arr4[i] << '\t';
 
 
 
 
+	const int size5 = 10;
+	int arr5[size5][size5];
+	srand(time(NULL));
+	
+	for (int i = 0; i < size5; i++)
+		for (int j = 0; j < size5; j++) {
+			arr5[i][j] = rand() % ;
 
-
+		}
 
 
 
